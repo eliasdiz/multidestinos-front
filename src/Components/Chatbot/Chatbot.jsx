8323bb473f9c70860417c6caaad9cd04ba02b0e8
eliasdiz <114044365+eliasdiz@users.noreply.github.com>
2024-12-23 12:26:47 -0500

    cuchobot (#3)

---
+++ src/Components/Chatbot/Chatbot.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CuchoBot from 'react-chatbot-kit'
+import 'react-chatbot-kit/build/main.css'
+
+export default function Chatbot() {
+    return (
+        <div>
+            <CuchoBot 
+
+            />
+        </div>
+    )
+}
