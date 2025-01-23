699ee792e849fcf647c781c7d64e575fa98c8096
elias <eliasj.diz@gmail.com>
2025-01-23 11:22:58 -0500

    Layout vista: tipo sidebar

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,13 +2,20 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../Pages/Home";
 import Login from "../components/Login";
 import DashBoard from "../components/DashBoard";
+import MainLayout from "../Layouts/MainLayout";
 
 
 const router = createBrowserRouter([
 
-    { path:'/', element: <Home />  },
-    { path:'/login', element: <Login />  },
-    { path:'/dashboard', element: <DashBoard />  },
+    {
+        path: '/', element: <MainLayout />,
+        children: [
+            { path:'/', element: <Home />  },
+            { path:'/login', element: <Login />  },
+            { path:'/dashboard', element: <DashBoard />  },
+        ]
+    }
+
 
 
 ])
